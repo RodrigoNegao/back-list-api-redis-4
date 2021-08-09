3a15890e26acbd24d385b315ac9b0c6adde7af29
Rodrigo da Cunha Pereira <RodrigoNegao@users.noreply.github.com>
2021-08-09 14:41:03 -0300

    redis online

--- src/core/infra/data/connections/redis.ts
+++ src/core/infra/data/connections/redis.ts
@@ -15,14 +15,14 @@ export class Redis {
   //https://dev.to/ramko9999/host-and-use-redis-for-free-51if
   //https://docs.redislabs.com/latest/rs/references/client_references/client_ioredis/
 
-  public async openConnection(): Promise<any> {
+  public async openConnection(): Promise<void> {
     if (!Redis.#connection) {
-      Redis.#connection = new IORedis(process.env.REDIS_URL);
-      // Redis.#connection = new IORedis({
-      //   host: process.env.REDIS_HOST,
-      //   port: process.env.REDIS_PORT,
-      //   password: process.env.REDIS_PASSWORD,
-      // });
+      //teste para localhost
+      //Redis.#connection = new IORedis(process.env.REDIS_URL);
+      const port = parseInt(process.env.REDIS_PORT!);
+      Redis.#connection = new IORedis(port, process.env.REDIS_HOST, {
+        password: process.env.REDIS_PASSWORD,
+      });
     }
   }
 }
